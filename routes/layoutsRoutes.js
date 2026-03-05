7dab05da7575c68e5af3532e2d4069f2a9099e98
flavia6346 <flavianicole@gmail.com>
2026-03-05 15:52:34 -0400

    arreglo

--- routes/layoutsRoutes.js
+++ routes/layoutsRoutes.js
@@ -1,11 +1,9 @@
 // backend/routes/layoutsRoutes.js
 const express = require('express');
 const router = express.Router();
-const { getModels } = require('../models/index.js');
 const { crearLayout, obtenerLayouts } = require('../controllers/layoutsController.js');
-const  uploadLayout  = require('../middleware/upload.js');
+const { uploadLayout } = require('../middleware/upload.js'); // ← llaves {}
 
-// Rutas
 router.post('/', uploadLayout.single('imagen'), crearLayout);
 router.get('/', obtenerLayouts);
 
