cd163add69bf47af022cf957384a6acd45cb920a
flavia6346 <flavianicole@gmail.com>
2026-05-05 22:30:58 -0400

    limpieza

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -34,7 +34,6 @@ const protect = async (req, res, next) => {
     }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    console.log('🔐 Token decodificado:', JSON.stringify(decoded, null, 2));
     const models = getModels();
     const User = models.User;
 
